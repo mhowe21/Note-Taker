5bc5acea1cc07b7b9e227056d64d9b0eb4c578d4
Mason A. Howe <mason.a.howe@gmail.com>
2021-01-30 19:37:40 -0700

    addID

--- Routes/API/APIRout.js
+++ Routes/API/APIRout.js
@@ -1,15 +1,25 @@
 const router = require("express").Router();
 const db = require("../../db/db.json");
+let id = 0;
 
 router.get("/notes", (req, res) => {
   res.json(db);
   console.log(db);
 });
 
-router.post("/notes", (req, res) => {
-  db.push(req.body);
-  res.json(db);
+router.post("/notes", mid, (req, res) => {
   console.log(db);
 });
+
+async function mid(req, res, next){
+
+  id = id +1;
+  let obj = {id: id}
+  db.push({...obj, ...req.body});
+  res.json(db);
+  next();
+
+
+}
 
 module.exports = router;
